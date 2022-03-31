1a6624c3a7e2f599053ef3f8834f9ff3bf648473
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2022-03-31 14:28:10 +0300

    added autho page

---
+++ Shop/bd_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class bd_connection
+    {
+        public static ShopEntities shop = new ShopEntities();
+    }
+}
