461b7436d5be0fd8b96dec8e0f1a80a6900d553e
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2022-04-01 11:07:49 +0300

    added auto page

--- Shop/AuthorizationPage.xaml.cs
+++ Shop/AuthorizationPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,20 @@ namespace Shop
     /// </summary>
     public partial class AuthorizationPage : Page
     {
+        public static ObservableCollection<User> users { get; set; }
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+
+        private void auto_btn_Click(object sender, RoutedEventArgs e)
+        {
+            users = new ObservableCollection<User>(bd_connection.shop.User.ToList());
+            var k = users.Where(a => a.Login == login_tbx.Text && a.Password == password_tbx.Password).FirstOrDefault();
+            if (k != null)
+                NavigationService.Navigate(new AuthorizationPage());
+            else
+                MessageBox.Show("Логин или пароль введены неверно", "error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
